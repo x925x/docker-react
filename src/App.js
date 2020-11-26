b84d899f1511b0267d04155b5fd61e3e0207cbeb
x925x <leonardo.gemini@gmail.com>
2020-11-26 13:29:09 -0500

    fixing more typos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	 Bye!!!! 
+	 I was changed in the fearture branch 
         </p>
         <a
           className="App-link"
